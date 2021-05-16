ea6b9bdf550ed3398941c88fe45b1637bece2617
Elisaveta Orlova <orlova.elizaveta1101@gmail.com>
2021-05-16 14:23:16 +0300

    Add test example

--- src/tests/calculating.test.js
+++ src/tests/calculating.test.js
@@ -16,3 +16,11 @@ test('Подсчет плана при билетах: 10 - 5, 20 - 8, 30 - 9, 6
         720: 0
     })).toBe(65);
   });
+
+  // test('adding positive numbers is not zero', () => {
+  //   for (let a = 1; a < 10; a++) {
+  //     for (let b = 1; b < 10; b++) {
+  //       expect(a + b).not.toBe(0);
+  //     }
+  //   }
+  // });
